52f636fc222dca7e6f780c34b328ff075a5d4f0d
keqiang <dukeqiang@qianshengqian.com>
2017-09-08 19:16:09 +0800

    修改HBase相关代码，新增SPARK-HIVE代码

--- src/main/scala/com/kq/WelcomScala.scala
+++ src/main/scala/com/kq/WelcomScala.scala
@@ -4,8 +4,7 @@ import org.apache.spark.sql.SparkSession
 import unicredit.spark.hbase.{HBaseConfig, _}
 
 /**
-  * 基于开源的 hbase-rdd 项目 实现的spark读写hbase的demo
-  * 附上 hbase-rdd github地址：https://github.com/unicredit/hbase-rdd
+  * Welcom You !!!
   */
 object WelcomScala {
   def main(args: Array[String]): Unit = {
---
+++ src/main/scala/com/kq/hive/SparkHiveBasic.scala
@@ -0,0 +1,44 @@
+package com.kq.hive
+
+import org.apache.spark.sql.SparkSession
+import unicredit.spark.hbase.{HBaseConfig, _}
+
+/**
+  * create user: keqiang.du
+  * desc: spark 读写 HIVE 的基础操作
+  */
+object SparkHiveBasic {
+  def main(args: Array[String]): Unit = {
+    // 设置提交任务的用户
+    System.setProperty("user.name", "root");
+
+    @transient implicit val config = HBaseConfig()
+    val warehouseLocation = "file:${system:user.dir}/spark-warehouse"
+    val spark = SparkSession
+      .builder()
+      .appName("SparkHiveBasic")
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()
+      .getOrCreate()
+
+    /**
+      * tableName: wahaha
+      * columnName: wa, ha, hah
+      */
+    val sql = "select * from wahaha"
+    val resultDataFrame = spark.sql(sql)
+    // 将 dataFrame 转成 RDD
+    val resultRDD = resultDataFrame.rdd
+    // resultRDD 保存到 HBase
+    val resultRDDTransfom = resultRDD.map({
+      case (row) =>
+        val col1val = row.getAs("wa").toString
+        val col2val = row.getAs("ha").toString
+        val col3val = row.getAs("hah").toString
+        val content = Map(
+          "hcol1" -> col1val, "hcol2" -> col2val, "hcol2" -> col3val
+        )
+        "rowKey" -> content
+    }).toHBase("hbase_table_name","info")
+  }
+}
